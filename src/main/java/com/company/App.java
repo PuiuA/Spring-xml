0eb6182fbfcec34ceb8b10acdcb7dff0cc54f634
crme291 <deeavinipu8@gmail.com>
2025-12-31 13:28:12 +0200

    repository, controller and service

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -8,6 +8,6 @@ public class App {
 
     public static void main(String[] args) {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("");
     }
 }
